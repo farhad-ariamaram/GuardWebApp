db996eac1e3317c7b1ca010f5bdf9874061dfe05
ym <ym@DESKTOP-2HUH2VU>
2021-06-20 09:59:44 +0430

    login page

--- GuardWebApp/Pages/Index.cshtml.cs
+++ GuardWebApp/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,16 +11,32 @@ namespace GuardWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        [BindProperty]
+        public userViewModel User { get; set; }
 
         public void OnGet()
         {
 
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Panel");
+        }
+    }
+
+    public class userViewModel
+    {
+        [Required(ErrorMessage ="نام کاربری اجباری است")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "رمز ورود اجباری است")]
+        public string Password { get; set; }
+
     }
 }
