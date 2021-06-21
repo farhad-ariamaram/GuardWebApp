392662351da8d1a47405dfacdcd3e383d1034553
ym <ym@DESKTOP-2HUH2VU>
2021-06-21 10:05:36 +0430

    locationpage complete

---
+++ GuardWebApp/Pages/LocationPage/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GuardWebApp.Models;
+
+namespace GuardWebApp.Pages.LocationPage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GuardWebApp.Models.GuardianDBContext _context;
+
+        public IndexModel(GuardWebApp.Models.GuardianDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Location> Location { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Location = await _context.Locations
+                .Include(l => l.GuardArea).ToListAsync();
+        }
+    }
+}
