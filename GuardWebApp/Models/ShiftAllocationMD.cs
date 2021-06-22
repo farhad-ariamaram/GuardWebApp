b5da1d4b72b4ee5787d08efa70c419bb052aa42e
ym <ym@DESKTOP-2HUH2VU>
2021-06-22 10:13:17 +0430

    shift allopcation metadata

---
+++ GuardWebApp/Models/ShiftAllocationMD.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardWebApp.Models
+{
+    public class ShiftAllocationMD
+    {
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        public DateTime DateTime { get; set; }
+    }
+
+    [ModelMetadataType(typeof(ShiftAllocationMD))]
+    public partial class ShiftAllocation
+    {
+    }
+}
