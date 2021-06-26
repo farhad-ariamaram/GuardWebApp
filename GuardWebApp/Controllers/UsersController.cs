d43cba323de2df56fd8f14a960b8c32622edccae
ym <ym@DESKTOP-2HUH2VU>
2021-06-26 11:46:20 +0430

    remove unused usings

--- GuardWebApp/Controllers/UsersController.cs
+++ GuardWebApp/Controllers/UsersController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using GuardWebApp.Models;
 using GuardWebApp.Controllers.Utils;
 using System.Net;
--- GuardWebApp/Models/GuardianDBContext.cs
+++ GuardWebApp/Models/GuardianDBContext.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
+
 
 #nullable disable
 
