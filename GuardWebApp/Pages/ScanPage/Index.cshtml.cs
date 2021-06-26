14a8e54eac9dda13a6901221d73c578d9f3d5f9d
ym <ym@DESKTOP-2HUH2VU>
2021-06-26 11:30:57 +0430

    remove unused usings

--- GuardWebApp/Pages/ScanPage/Index.cshtml.cs
+++ GuardWebApp/Pages/ScanPage/Index.cshtml.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using GuardWebApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ZXing.QrCode;
 
 namespace GuardWebApp.Pages.ScanPage
 {
