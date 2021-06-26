60f06f8c86fcbf6a887dd0c19669e39209b85323
ym <ym@DESKTOP-2HUH2VU>
2021-06-26 11:01:19 +0430

    change UI UX

---
+++ GuardWebApp/Pages/Definations.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GuardWebApp.Pages
+{
+    public class DefinationsModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            var uid = HttpContext.Session.GetString("uid");
+            if (uid == null)
+            {
+                return RedirectToPage("../Index");
+            }
+
+            return Page();
+        }
+    }
+}
