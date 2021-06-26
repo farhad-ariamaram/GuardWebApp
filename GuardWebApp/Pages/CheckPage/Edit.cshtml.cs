2356876871e8163269be122895914f1256bd599b
ym <ym@DESKTOP-2HUH2VU>
2021-06-26 09:52:40 +0430

    planning with no where load all in 8 steps

--- GuardWebApp/Pages/CheckPage/Edit.cshtml.cs
+++ GuardWebApp/Pages/CheckPage/Edit.cshtml.cs
@@ -45,8 +45,6 @@ namespace GuardWebApp.Pages.CheckPage
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
--- GuardWebApp/Pages/Planning/Index.cshtml.cs
+++ GuardWebApp/Pages/Planning/Index.cshtml.cs
@@ -2,13 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GuardWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GuardWebApp.Pages.Planning
 {
     public class IndexModel : PageModel
     {
+        private readonly GuardianDBContext _context;
+
+        public IndexModel(GuardianDBContext context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult OnGet()
         {
             ViewData["step1"] = true;
@@ -18,12 +28,48 @@ namespace GuardWebApp.Pages.Planning
         public IActionResult OnPostDate(DateTime sDate, DateTime eDate)
         {
             ViewData["step2"] = true;
+            ViewData["GuardsId"] = new SelectList(_context.Users, "Id", "Name");
             return Page();
         }
 
         public IActionResult OnPostGuards(int guardId)
         {
             ViewData["step3"] = true;
+            ViewData["GuardAreasId"] = new SelectList(_context.GuardAreas, "Id", "Description");
+            return Page();
+        }
+
+        public IActionResult OnPostGuardAreas(int guardAreaId)
+        {
+            ViewData["step4"] = true;
+            ViewData["ShiftsId"] = new SelectList(_context.Shifts, "Id", "DateTime");
+            return Page();
+        }
+
+        public IActionResult OnPostShifts(int shiftId)
+        {
+            ViewData["step5"] = true;
+            ViewData["LocationsId"] = new SelectList(_context.Locations, "Id", "Name");
+            return Page();
+        }
+
+        public IActionResult OnPostLocations(int locationId)
+        {
+            ViewData["step6"] = true;
+            ViewData["RhythmsId"] = new SelectList(_context.Rhythms, "Id", "Title");
+            return Page();
+        }
+
+        public IActionResult OnPostRhythms(int rhythmId)
+        {
+            ViewData["step7"] = true;
+            ViewData["RhythmDetailsId"] = new SelectList(_context.RhythmDetails, "Id", "Time");
+            return Page();
+        }
+
+        public IActionResult OnPostRhythmDetails(int rhythmDetailId)
+        {
+            ViewData["step8"] = true;
             return Page();
         }
     }
