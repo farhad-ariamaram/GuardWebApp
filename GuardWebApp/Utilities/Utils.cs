8829eb059851ad572996744ec745362e74f56121
ym <ym@DESKTOP-2HUH2VU>
2021-06-27 12:59:43 +0430

    changes

--- GuardWebApp/Utilities/Utils.cs
+++ GuardWebApp/Utilities/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GuardWebApp.Utilities
 {
@@ -7,6 +8,12 @@ namespace GuardWebApp.Utilities
         public static string removeYear(this DateTime datetime)
         {
             return string.Format("{0}/{1}", datetime.Month, datetime.Day);
+        }
+
+        public static IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+                yield return day;
         }
     }
 }
