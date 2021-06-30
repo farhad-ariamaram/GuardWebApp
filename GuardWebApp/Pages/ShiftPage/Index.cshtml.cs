78654499806a603c36bef7fd3a57868baa318853
ym <ym@DESKTOP-2HUH2VU>
2021-06-30 16:21:20 +0430

    random shift page complete

--- GuardWebApp/Pages/ShiftPage/Index.cshtml.cs
+++ GuardWebApp/Pages/ShiftPage/Index.cshtml.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GuardWebApp.Models;
 using Microsoft.AspNetCore.Http;
+using System;
+using GuardWebApp.Utilities;
+using System.Globalization;
+using System.Linq;
 
 namespace GuardWebApp.Pages.ShiftPage
 {
@@ -17,7 +21,7 @@ namespace GuardWebApp.Pages.ShiftPage
             _context = context;
         }
 
-        public IList<Shift> Shift { get;set; }
+        public IList<Shift> Shift { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -36,6 +40,36 @@ namespace GuardWebApp.Pages.ShiftPage
 
         public IActionResult OnPostAuto(string sdayField, string smonthField, string edayField, string emonthField)
         {
+            PersianCalendar pc = new PersianCalendar();
+
+            var guardAreasList = _context.GuardAreas.ToList();
+
+            foreach (DateTime day in Utils.EachDay(new DateTime(1400, int.Parse(smonthField), int.Parse(sdayField), pc), new DateTime(1400, int.Parse(emonthField), int.Parse(edayField), pc)))
+            {
+                foreach (var item in guardAreasList)
+                {
+                    var rhytms = _context.Rhythms.Where(a => a.GuardAreaId == item.Id).ToList();
+                    if (rhytms.Any())
+                    {
+                        Random rnd = new Random();
+                        int randomRythm = rnd.Next(rhytms.Count);
+                        Models.Shift shift = new Models.Shift()
+                        {
+                            DateTime = day,
+                            GuardAreaId = item.Id,
+                            RhythmId = rhytms[randomRythm].Id
+                        };
+                        _context.Shifts.Add(shift);
+                        
+                    }
+                }
+            }
+            _context.SaveChanges();
+
+            Shift = _context.Shifts
+                .Include(s => s.GuardArea)
+                .Include(s => s.Rhythm).ToList();
+
             return Page();
         }
     }
