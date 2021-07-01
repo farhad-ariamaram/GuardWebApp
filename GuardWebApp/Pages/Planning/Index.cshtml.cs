cffbe3f05b7c866e1f2aefbb15ede95a4e8cd161
ym <ym@DESKTOP-2HUH2VU>
2021-07-01 09:00:02 +0430

    make planning logic persian

--- GuardWebApp/Pages/Planning/Index.cshtml.cs
+++ GuardWebApp/Pages/Planning/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using GuardWebApp.Models;
 using GuardWebApp.Utilities;
@@ -48,21 +49,34 @@ namespace GuardWebApp.Pages.Planning
             return Page();
         }
 
-        public IActionResult OnPostDate(string sDate, string eDate, string timetype)
+        public IActionResult OnPostDate(string sDate, string timetype)
         {
+            PersianCalendar pc = new PersianCalendar();
+
             switch (timetype)
             {
                 case "tofrom":
-                    startDate = Convert.ToDateTime(sDate);
-                    endDate = Convert.ToDateTime(eDate);
+                    startDate = new System.DateTime(1400, int.Parse(Request.Form["smonthField"].ToString()), int.Parse(Request.Form["sdayField"].ToString()), pc);
+                    endDate = new System.DateTime(1400, int.Parse(Request.Form["emonthField"].ToString()), int.Parse(Request.Form["edayField"].ToString()), pc);
                     break;
                 case "daily":
-                    startDate = Convert.ToDateTime(sDate);
+                    startDate = new System.DateTime(1400, int.Parse(Request.Form["monthField"].ToString()), int.Parse(Request.Form["dayField"].ToString()), pc);
                     endDate = startDate;
                     break;
                 case "monthly":
-                    startDate = new DateTime(2000, int.Parse(sDate), 1);
-                    endDate = new DateTime(2000, int.Parse(sDate), DateTime.DaysInMonth(2000, int.Parse(sDate)));
+                    startDate = new DateTime(1400, int.Parse(sDate), 1, pc);
+                    int lastdaymonth = 29;
+                    int[] first6month = { 1, 2, 3, 4, 5, 6 };
+                    int[] second6month = { 7, 8, 9, 10, 11 };
+                    if (first6month.Contains(int.Parse(sDate)))
+                    {
+                        lastdaymonth = 31;
+                    }
+                    else if (second6month.Contains(int.Parse(sDate)))
+                    {
+                        lastdaymonth = 30;
+                    }
+                    endDate = new DateTime(1400, int.Parse(sDate), lastdaymonth, pc);
                     break;
                 default:
                     break;
@@ -90,52 +104,52 @@ namespace GuardWebApp.Pages.Planning
                         if (_context.Shifts.Where(a => a.DateTime.Month == day.Month && a.DateTime.Day == day.Day && a.GuardAreaId == guardArea && a.RhythmId == rhythm).Any())
                         {
                             shift = _context.Shifts.FirstOrDefault(a => a.DateTime.Month == day.Month && a.DateTime.Day == day.Day && a.GuardAreaId == guardArea && a.RhythmId == rhythm).Id;
-                        }
-                        else
-                        {
-                            Shift shift1 = new Shift
+
+                            //در این مرحله حضور نگهبان در این روز را بوسیله آی دی نگهبان که در بالا بدست اوردیم و روز(دٍی) میگیریم
+                            //در اینجا چون ای پی ای اماده نبود از دیتا این مموری استفاده کردیم
+                            List<AttendanceTime> attendanceTimes = new List<AttendanceTime>
                             {
-                                GuardAreaId = guardArea,
-                                RhythmId = rhythm,
-                                DateTime = new DateTime(2000, day.Month, day.Day)
+                                new AttendanceTime{ StartDate = "2021-03-21 08:00:00",EndDate = "2021-03-21 16:30:00", leave="false"},
+                                new AttendanceTime{ StartDate = "2021-03-22 09:00:00",EndDate = "2021-03-22 16:30:00", leave="false"},
+                                new AttendanceTime{ StartDate = "2021-03-23 09:55:00",EndDate = "2021-03-23 16:30:00", leave="false"}
                             };
 
-                            _context.Shifts.Add(shift1);
-                            _context.SaveChanges();
-
-                            shift = shift1.Id;
-                        }
-
-                        //در این مرحله حضور نگهبان در این روز را بوسیله آی دی نگهبان که در بالا بدست اوردیم و روز(دٍی) میگیریم
-                        //در اینجا چون ای پی ای اماده نبود از دیتا این مموری استفاده کردیم
-                        List<AttendanceTime> attendanceTimes = new List<AttendanceTime>
-                        {
-                            new AttendanceTime{ StartDate = "2021-06-01 08:00:00",EndDate = "2021-06-01 09:00:00", leave="false"},
-                            new AttendanceTime{ StartDate = "2021-06-01 09:00:00",EndDate = "2021-06-01 09:55:00", leave="false"},
-                            new AttendanceTime{ StartDate = "2021-06-01 09:55:00",EndDate = "2021-06-01 16:30:00", leave="false"}
-                        };
-
-                        foreach (var attendanceTime in attendanceTimes)
-                        {
-                            if (time >= new TimeSpan(DateTime.Parse(attendanceTime.StartDate).Hour, DateTime.Parse(attendanceTime.StartDate).Minute, DateTime.Parse(attendanceTime.StartDate).Second) &&
-                                time < new TimeSpan(DateTime.Parse(attendanceTime.EndDate).Hour, DateTime.Parse(attendanceTime.EndDate).Minute, DateTime.Parse(attendanceTime.EndDate).Second) &&
-                                attendanceTime.leave == "false")
+                            foreach (var attendanceTime in attendanceTimes)
                             {
-                                Plan plan = new Plan
+                                if (time >= new TimeSpan(DateTime.Parse(attendanceTime.StartDate).Hour, DateTime.Parse(attendanceTime.StartDate).Minute, DateTime.Parse(attendanceTime.StartDate).Second) &&
+                                    time < new TimeSpan(DateTime.Parse(attendanceTime.EndDate).Hour, DateTime.Parse(attendanceTime.EndDate).Minute, DateTime.Parse(attendanceTime.EndDate).Second) &&
+                                    attendanceTime.leave == "false")
                                 {
-                                    UserId = guard,
-                                    LocationId = location,
-                                    ShiftId = shift,
-                                    DateTime = new DateTime(2000, day.Month, day.Day, time.Hours, time.Minutes, time.Seconds)
-                                };
-                                if (!_context.Plans.Where(a => a.UserId == guard && a.LocationId == location && a.ShiftId == shift && a.DateTime == new DateTime(2000, day.Month, day.Day, time.Hours, time.Minutes, time.Seconds)).Any())
-                                {
-                                    _context.Plans.Add(plan);
-                                    _context.SaveChanges();
-                                }
+                                    Plan plan = new Plan
+                                    {
+                                        UserId = guard,
+                                        LocationId = location,
+                                        ShiftId = shift,
+                                        DateTime = new DateTime(1400, day.Month, day.Day, time.Hours, time.Minutes, time.Seconds, pc)
+                                    };
+                                    if (!_context.Plans.Where(a => a.UserId == guard && a.LocationId == location && a.ShiftId == shift && a.DateTime == new DateTime(1400, day.Month, day.Day, time.Hours, time.Minutes, time.Seconds, pc)).Any())
+                                    {
+                                        _context.Plans.Add(plan);
+                                        _context.SaveChanges();
+                                    }
 
+                                }
                             }
                         }
+                        //else
+                        //{
+                        //    Shift shift1 = new Shift
+                        //    {
+                        //        GuardAreaId = guardArea,
+                        //        RhythmId = rhythm,
+                        //        DateTime = new DateTime(2000, day.Month, day.Day)
+                        //    };
+
+                        //    _context.Shifts.Add(shift1);
+                        //    _context.SaveChanges();
+
+                        //    shift = shift1.Id;
+                        //}
                     }
                 }
             }
